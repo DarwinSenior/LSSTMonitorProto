df5ee03110eddecf2579f1a9b5884101c0a0cd78
darwinsenior <darwinsenior@gmail.com>
2015-06-11 12:47:54 -0500

    Second experiment. Trying to server files from firefly demo

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def intropage():
+	return redirect("/index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000, debug=True)
